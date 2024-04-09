16c017a71dcead4c9c017ed6d967a400ccd5bb92
someshwargithub <som06_1980@yahoo.com>
2024-04-09 09:40:06 +0530

    Non Angular latest commit

---
+++ Somesh-Sample-Non-Angular/app.js
@@ -0,0 +1,6 @@
+const reasonInput = document.querySelector("#input-reason");
+const amountInput = document.querySelector("#input-amount");
+const confirmBtn = document.querySelector("#input-add");
+confirmBtn.addEventListener("click",()=>{
+    console.log ("it works....!");
+});
